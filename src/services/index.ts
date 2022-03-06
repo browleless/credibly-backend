1d445a56ddfcf803a20cd1c7b9169734a51b7a7e
Jefferson <jefflowzh@live.com>
2022-03-07 01:41:25 +0800

    Implement upload image function

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,8 +2,10 @@ import { AuthService } from './AuthService';
 import { AwardeeService } from './AwardeeService';
 import { AwardeeGroupService } from './AwardeeGroupService';
 import { UserService } from './UserService';
+import { CredentialTemplateService } from './CredentialTemplateService';
 
 export const authService = new AuthService();
 export const awardeeService = new AwardeeService();
 export const awardeeGroupService = new AwardeeGroupService();
 export const userService = new UserService();
+export const credentialTemplateService = new CredentialTemplateService();
