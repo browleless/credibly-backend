3973330751cb0da3a8b744a468744f05fc318936
amqndalai <e0325976@u.nus.edu>
2022-03-27 20:54:33 +0800

    edited table to remove certificateUuid

--- src/scripts/tables.sql
+++ src/scripts/tables.sql
@@ -50,7 +50,6 @@ CREATE TABLE IF NOT EXISTS `TransferRequest` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
   `organisationId` int(11) NOT NULL,
-  `certificateUuid` varchar(255) NOT NULL,
   `transferTo` varchar(255) NOT NULL,
   `approved` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
